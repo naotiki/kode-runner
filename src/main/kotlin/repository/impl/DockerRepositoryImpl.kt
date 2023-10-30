8e64adc323aa8c6b7e2d83c4760abcd78a3a6891
Naotiki <contact@naotiki.me>
2023-10-30 23:04:36 +0900

    chore: add ktx serialization

--- src/main/kotlin/repository/impl/DockerRepositoryImpl.kt
+++ src/main/kotlin/repository/impl/DockerRepositoryImpl.kt
@@ -1,12 +1,8 @@
 package repository.impl
 
 import com.github.dockerjava.api.DockerClient
-import com.github.dockerjava.api.async.ResultCallback
-import com.github.dockerjava.api.async.ResultCallback.Adapter
-import com.github.dockerjava.api.async.ResultCallbackTemplate
 import com.github.dockerjava.api.model.HostConfig
 import com.github.dockerjava.api.model.Image
-import com.github.dockerjava.api.model.Statistics
 import repository.DockerRepository
 
 class DockerRepositoryImpl(private val dockerApi:DockerClient) :DockerRepository {
